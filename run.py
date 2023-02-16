5929457172edc62cf8037bd0d7bde8276e84ef99
Paul Trotter <paul.trotter9@gmail.com>
2023-02-16 06:26:18 +0000

    Tidied up code

--- run.py
+++ run.py
@@ -133,8 +133,9 @@ def calculate_stock_data(data):
         average = sum(int_column) / len(int_column)
         stock_num = average * 1.1
         new_stock_data.append(round(stock_num))
-    
+
     return new_stock_data
+
 
 def main():
     """
@@ -148,6 +149,7 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+
 
 print("Welcome to Love Sandwiches Data Automation\n")
 main()
